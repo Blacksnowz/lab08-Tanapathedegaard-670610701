bc48f070709a6c768fd304379251e35ab3cc9b66
Blacksnowz <flaxiaroz@gmail.com>
2025-08-02 17:28:38 +0700

    test1

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+import type { FooterProps } from "../libs/types";
+const Footer = ({}: FooterProps) => (
+<footer className="text-secondary text-center p-2 bg-light mt-auto">
+        Copyright © 2026 Tanapat Hedegaard 670610701
+      </footer>
+)
+export default Footer;
---
+++ components/Header.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+const Header = () => (
+    <header className="text-white bg-primary p-2 w-100">
+        <h4>Note App</h4>
+    </header>
+);
+export default Header;
